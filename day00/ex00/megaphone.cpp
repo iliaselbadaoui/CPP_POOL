0df2a53dfd9eaa4c86da1a3baa20597d78b0394a
Ilias Elbadaoui <ielbadao@e0r4p5.1337.ma>
2021-06-22 20:54:44 +0100

    new

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -5,13 +5,17 @@ int		main(int argc, char **argv)
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else if (argc == 2)
+	else
 	{
-		for (int i=0; argv[1][i]; i++)
+
+		for (int j = 0; argv[j]; j++)
 		{
-			std::putchar(std::toupper(argv[1][i]));
+			for (int i=0; argv[j][i]; i++)
+			{
+				std::putchar(std::toupper(argv[j][i]));
+			}
+			std::cout << std::endl;
 		}
-		std::cout << std::endl;
 	}
 	return 0;
 }
