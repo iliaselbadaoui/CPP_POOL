46ea3fdacb8d5d705b7ba7ff6d4b33baa33b30a3
iliaselbadaoui <iliassovich@outlook.com>
2021-06-25 18:51:32 +0100

    day01 near push

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -24,24 +24,37 @@ std::string	replace(std::string line, std::string str, std::string substitute)
 
 int	main(int argc, char **argv)
 {
-	std::ifstream f(argv[1]);
+	std::ifstream input(argv[1]);
 	std::string content;
 	std::string line;
 	std::string fname = std::string(argv[1]) + std::string(".replace");
-	std::ofstream r(fname);
+	std::ofstream output(fname);
+	int		empty = 1;
 
+	if (input.fail())
+	{
+		std::cout << "NO SUCH FILE '"<< argv[1] << "'" << std::endl;
+		return 0;
+	}
 	if (argc != 4)
 	{
 		std::cout << "the programe must take three args <FILENAME> <STRING> <SUBSTITUTE>" << std::endl;
 		return 1;
 	}
 	content = "";
-	while (std::getline(f, line))
+	while (std::getline(input, line))
 	{
+		if (empty)
+			empty = 0;
 		line = replace(line, std::string(argv[2]), std::string(argv[3]));
 		content += line;
 		content.push_back('\n');
 	}
-	r << content;
+	if (empty)
+	{
+		std::cout << "EMPTY FILE!" << std::endl;
+		return 0;
+	}
+	output << content;
 	return 0;
 }
