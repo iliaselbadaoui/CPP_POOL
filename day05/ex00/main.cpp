98ef958f24a8d40fc61c201637f7502ca57cecf5
Ilias Elbadaoui <iliassovich@outlook.com>
2021-07-02 09:45:06 +0100

    auto commit

--- day05/ex00/main.cpp
+++ day05/ex00/main.cpp
@@ -2,14 +2,21 @@
 
 int main()
 {
+	Bureaucrat bureau("Ilias", 10);
+	std::cout << bureau << std::endl;
+	std::cout << "WITH TRY-CATCH BLOCKS PRODUCES NO EXCEPTION." << std::endl;
 	try
 	{
-		Bureaucrat bureau("Ilias", 1);
-		bureau.increment();
+		for (int i = 0; i < 10; i++)
+		{
+			bureau.increment();
+		}
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+	std::cout << "WITHOUT TRY-CATCH BLOCKS PRODUCES EXCEPTION AND ABORT THE PROGRAMME AFTER THAT." << std::endl << std::endl;
+	bureau.increment();
 	return 0;
 }
